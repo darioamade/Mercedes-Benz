7724a5567cd27e16128f1421e7a4fc07c1bd803e
darioamade <dario_isd@outlook.com>
2020-08-16 19:55:32 +0100

    commit
    new files added

--- js/app.js
+++ js/app.js
@@ -83,3 +83,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+
+const menuOpen = document.querySelector('.menu-open');
+const menuClose = document.querySelector('.menu-close');
+const nav = document.querySelector('.nav1');
+
+menuOpen.addEventListener('click', menuTog);
+menuClose.addEventListener('click', menuTog);
+
+function menuTog(e){
+  nav.classList.toggle('active')
+};
